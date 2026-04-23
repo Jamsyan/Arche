f2e5b0c9ede83472cd2819f1e31c3050caaa6f82
Jamsyan <jihanyang123@163.com>
2026-04-23 21:24:42 +0800

    fix: 修复格式化问题

--- backend/migrations/versions/004_config_entries.py
+++ backend/migrations/versions/004_config_entries.py
@@ -30,7 +30,9 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     with op.batch_alter_table("config_entries", schema=None) as batch_op:
-        batch_op.create_index(batch_op.f("ix_config_entries_group"), ["group"], unique=False)
+        batch_op.create_index(
+            batch_op.f("ix_config_entries_group"), ["group"], unique=False
+        )
         batch_op.create_index(batch_op.f("ix_config_entries_key"), ["key"], unique=True)
 
 
